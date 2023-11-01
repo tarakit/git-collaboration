5587f32b7e1738242f8ce6edc3bef6713085fa95
Tara Kit <darakit069@gmail.com>
2023-11-01 17:09:45 +0700

    fix console display

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
+
+    static void doSomething(){
+        System.out.println("Do something here...");
+    }
     public static void main(String[] args) {
         System.out.println("From Roza");
-        System.out.println("Hello world!");
+        doSomething();
     }
 }
