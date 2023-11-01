080f3a5dea359093c4a7cb15d89724e23ecbac7f
Tara Kit <darakit069@gmail.com>
2023-11-01 17:06:04 +0700

    add new feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+
+    void printError(){
+        System.out.println("print error ....");
+    }
     public static void main(String[] args) {
         System.out.println("From Roza");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello world!");
     }
 }
